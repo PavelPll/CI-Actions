327680cc3a51f99cb57e49c9d7434dd3c884bcba
PavelPll <51364937+PavelPll@users.noreply.github.com>
2023-11-23 15:57:31 +0100

    Update vendingmachine_test.py

--- CI_CD_githubActions/vendingmachine_test.py
+++ CI_CD_githubActions/vendingmachine_test.py
@@ -1,20 +1,20 @@
 from vendingmachine import VendingMachine, Jus, DataCola, noFreeSpaceInVendingMachine
 import pytest
 
+# Initialize two drink instances jus1 and datacola1
 jus1 = Jus(1000)
-
 @pytest.fixture
 def datacola1():
     return(DataCola('can'))
 datacola1 = DataCola('can')
 
-# I: Test drink expiration
+# I: Test drink expiration for datacola1
 def test_drinkExpiration():
-    days = datacola1.daysBeforeExpiration
+    days_left = datacola1.daysBeforeExpiration
     datacola1.addNewDay()
-    assert(datacola1.daysBeforeExpiration) == days - 1
+    assert(datacola1.daysBeforeExpiration) == days_left - 1
 
-# Create and fill in vendingmachine 100% with drinks (10 places for drinks)
+# Create vendingmachine and fill it in with drinks (add 10 drinks to 10 places for drinks)
 vendingmachine1 = VendingMachine()
 for nDrinks in range(5):
     vendingmachine1.addDrink(Jus(1000))
@@ -25,10 +25,21 @@ def test_freeSpaceForDrinks():
     with pytest.raises(noFreeSpaceInVendingMachine):
         vendingmachine1.addDrink(Jus(1000))
 
-# III: Check if all expired in 20 days drinks are removed
-def test_removeExpiredDrinks():
-    for i in range (20):
+# III: Check if all drinks out of 10, expired in a certain number of days, are removed
+@pytest.mark.parametrize("days,NOTremoved",[(1,10),(8,5),(61,0)])
+# Jus(1000) expired after 7 days
+# DataCola('can') expired after 60 days
+def test_removeExpiredDrinks(days, NOTremoved):
+    vendingmachine2 = VendingMachine()
+    # add 10 drinks to vendingmachine
+    for nDrinks in range(5):
+        vendingmachine2.addDrink(Jus(1000)) # expired after 7 days
+        vendingmachine2.addDrink(DataCola('can')) # expired after 60 days
+    # wait for days=days days
+    for i in range (days):
         for j in range( vendingmachine1.numberOfDrinksInside ):
-            vendingmachine1.contentOfVendingMachine[j].addNewDay()
-    vendingmachine1.autoRemoveExpiredDrinks()
-    assert(vendingmachine1.numberOfDrinksInside) == 5
+            vendingmachine2.contentOfVendingMachine[j].addNewDay()
+    # remove expired drinks
+    vendingmachine2.autoRemoveExpiredDrinks()
+    # check whether all expired drinks are removed
+    assert(vendingmachine2.numberOfDrinksInside) == NOTremoved
